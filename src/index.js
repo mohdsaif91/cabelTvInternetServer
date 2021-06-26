1b5406ad1bb72fae15e0c31b81e165843c8567bd
MohdSaif <MohdSaif@nextgen.com>
2021-06-26 17:54:23 +0530

    final try

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 const app = require('./app');
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 const port = process.env.PORT || 5000;
 app.listen(port, async () => {
+	console.log(process.env.PORT, '<>', process.env.mongoDBURL);
 	await mongoose
-		.connect(process.env.mongoDBURL, { useNewUrlParser: true, useUnifiedTopology: true })
+		.connect(process.env.mongoDBURL, {
+			useNewUrlParser: true,
+			useCreateIndex: true,
+			useUnifiedTopology: true,
+		})
 		.then((db, err) => {
 			if (db) {
 				console.log(
