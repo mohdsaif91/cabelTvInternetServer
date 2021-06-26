fcb809402724a38f5445517593477269317c759d
MohdSaif <MohdSaif@nextgen.com>
2021-06-26 16:56:36 +0530

    testing DB2

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ const app = require('./app');
 const mongoose = require('mongoose');
 
 const port = process.env.PORT || 5000;
-app.listen(port, () => {
-	mongoose
+app.listen(port, async () => {
+	await mongoose
 		.connect(process.env.mongoDBURL, { useNewUrlParser: true, useUnifiedTopology: true })
 		.then((db, err) => {
 			if (db) {
